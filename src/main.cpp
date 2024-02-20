2a7f5a72ddaf96b0e840bde13fe254b88e3afe1e
tony-97 <138673212+tony-97@users.noreply.github.com>
2024-02-20 18:31:03 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <raylib.h>
+
+auto
+main() -> int
+{
+  InitWindow(640, 480, "Window");
+  SetTargetFPS(60);
+  while (not WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawFPS(10, 10);
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
+}
